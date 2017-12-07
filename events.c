ab7ed535468b3001b589c0f5b66b465db894b170
VictorBaudot <vbaudot@student.42.fr>
2017-12-07 16:56:04 +0100

    Y

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: vbaudot <vbaudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/03 14:00:46 by vbaudot           #+#    #+#             */
-/*   Updated: 2017/12/07 16:49:21 by vbaudot          ###   ########.fr       */
+/*   Updated: 2017/12/07 16:55:49 by vbaudot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	my_key_funct(int keycode, t_data *data)
 	if (keycode == 53)
 	{
 		mlx_destroy_image(data->mlx, data->img.img_ptr);
-        mlx_destroy_window(data->mlx, data->win);
+		mlx_destroy_window(data->mlx, data->win);
 		exit(EXIT_SUCCESS);
 	}
 	return (0);
